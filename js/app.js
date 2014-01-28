f020a7c631ea4d5b1cc7dfd43399fcfd0f736c0d
Keith Anderson <keithanderson@boldier.com>
2014-01-27 20:12:36 -0800

    Version 1.1
    This is my first try so far. Let me know what you think!

---
+++ js/app.js
@@ -0,0 +1,52 @@
+$(document).ready(function() {
+	
+	$('.ryu').mouseenter(function(){
+		$('.ryu-still').hide();
+		$('.ryu-ready').show();
+	})	
+	.mouseleave(function(){
+		$('.ryu-ready').hide();
+		$('.ryu-still').show();
+	})
+	.mousedown(function(){
+		// play hadouken sound
+	//	playHadouken();
+		// show hadouken and animate it to the right
+		$('.ryu-ready').hide();
+		$('.ryu-throwing').show();
+		$('.hadouken').finish().show()
+		.animate(
+			{'left' : '300px'}, 
+			600, 
+			function(){
+			$(this).hide();
+			$(this).css({'left': '-212px'});
+		}
+		);
+	})
+	.mouseup(function(){
+		console.log('mouseup');
+		$('.ryu-throwing').hide();
+		$('.ryu-ready').show();
+	});
+	$(document).keydown(function(event) {
+		if (event.which == 88) {
+		   event.preventDefault();
+			$('.ryu-ready').hide();
+			$('.ryu-cool').show();
+		}
+	})
+	.keyup(function(event) {
+		$('.ryu-cool').hide();
+		$('.ryu-ready').show();
+
+	});
+
+	$('#directions').show().delay(3000).fadeOut();
+});
+
+function playHadouken () {
+  $('#hadouken-sound')[0].volume = .5;
+  $('#hadouken-sound')[0].load();
+  $('#hadouken-sound')[0].play();
+}
