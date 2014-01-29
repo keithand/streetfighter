bf762dcb84e2300e76164cc25d89968ed0b8420f
Keith Anderson <keithanderson@boldier.com>
2014-01-28 21:04:25 -0800

    version 1.4
    latest version

--- js/app.js
+++ js/app.js
@@ -1,49 +1,49 @@
 $(document).ready(function() {
-	
-	$('.ryu').mouseenter(function(){
-		$('.ryu-still').hide();
-		$('.ryu-ready').show();
-	})	
-	.mouseleave(function(){
-		$('.ryu-ready').hide();
-		$('.ryu-still').show();
-	})
-	.mousedown(function(){
-		// play hadouken sound
-		playHadouken();
-		// show hadouken and animate it to the right
-		$('.ryu-ready').hide();
-		$('.ryu-throwing').show();
-		$('.hadouken').finish().show()
-		.animate(
-			{'left' : '300px'}, 
-			600, 
-			function(){
-			$(this).hide();
-			$(this).css({'left': '-212px'});
-		}
-		);
-	})
-	.mouseup(function(){
-		console.log('mouseup');
-		$('.ryu-throwing').hide();
-		$('.ryu-ready').show();
-	});
-	$(document).keydown(function(event) {
-		if (event.which == 88) {
-			playSexy ();
-		   event.preventDefault();
-			$('.ryu-ready').hide();
-			$('.ryu-cool').show();
-		}
-	})
-	.keyup(function(event) {
-		$('.ryu-cool').hide();
-		$('.ryu-ready').show();
+        
+        $('.ryu').mouseenter(function(){
+                $('.ryu-still').hide();
+                $('.ryu-ready').show();
+        })        
+        .mouseleave(function(){
+                $('.ryu-ready').hide();
+                $('.ryu-still').show();
+        })
+        .mousedown(function(){
+                // play hadouken sound
+                playHadouken();
+                // show hadouken and animate it to the right
+                $('.ryu-ready').hide();
+                $('.ryu-throwing').show();
+                $('.hadouken').finish().show()
+                .animate(
+                        {'left' : '300px'}, 
+                        600, 
+                        function(){
+                        $(this).hide();
+                        $(this).css({'left': '-212px'});
+                }
+                );
+        })
+        .mouseup(function(){
+                console.log('mouseup');
+                $('.ryu-throwing').hide();
+                $('.ryu-ready').show();
+        });
+        $(document).keydown(function(event) {
+                if (event.which == 88) {
+                        playSexy ();
+                   event.preventDefault();
+                        $('.ryu-ready').hide();
+                        $('.ryu-cool').show();
+                }
+        })
+        .keyup(function(event) {
+                $('.ryu-cool').hide();
+                $('.ryu-ready').show();
 
-	});
+        });
 
-	$('#directions').show().delay(3000).fadeOut();
+        $('#directions').show().delay(3000).fadeOut();
 });
 
 function playHadouken () {
@@ -57,7 +57,5 @@ function playSexy () {
   $('.sexy')[0].load();
   $('.sexy')[0].play();
 }
-
-
 
 
