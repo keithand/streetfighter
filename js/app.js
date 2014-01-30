9314bce12485e6e81eb44b370347987752f647b5
Keith Anderson <keithanderson@boldier.com>
2014-01-29 20:49:34 -0800

    Version 1.6

--- js/app.js
+++ js/app.js
@@ -1,54 +1,87 @@
 $(document).ready(function() {
 
+//**************FUNCTIONS*****************
+
 function playCool () {
     if (event.which == 88) {
     playSexy ();
-    $('ryu-ready').hide();
+    $('.ryu-ready').hide();
     $('.ryu-cool').show();   
     }
 }
 
+function playCoolFinish () {
+    $('.ryu-cool').hide();
+    $('.ryu-ready').show();
+}
 
+function playReady () {
+   $('.ryu-still').hide();
+   $('.ryu-ready').show();
+}
+
+function playStill () {
+  $('.ryu-ready').hide();
+  $('.ryu-still').show();
+}
+
+function playThrowing () {
+  $('.ryu-ready').hide();
+  $('.ryu-throwing').show();
+  $('.hadouken').finish().show()
+  .animate(
+    {'left' : '300px'}, 
+    600, 
+    function(){
+    $(this).hide();
+    $(this).css({'left': '-212px'});
+    });
+}
+
+function playThrowingFinish () {
+      console.log('mouseup');
+    $('.ryu-throwing').hide();
+    $('.ryu-ready').show();  
+}
+
+function directions () {
+    $('#directions').show().delay(3000).fadeOut();
+
+}
+
+// *******************MAIN EVENTS******************
         $('.ryu').mouseenter(function(){
-                $('.ryu-still').hide();
-                $('.ryu-ready').show();
+            playReady ();
         })        
+        
         .mouseleave(function(){
-                $('.ryu-ready').hide();
-                $('.ryu-still').show();
+            playStill ();
         })
+        
         .mousedown(function(){
                 // play hadouken sound
                 playHadouken();
                 // show hadouken and animate it to the right
-                $('.ryu-ready').hide();
-                $('.ryu-throwing').show();
-                $('.hadouken').finish().show()
-                .animate(
-                        {'left' : '300px'}, 
-                        600, 
-                        function(){
-                        $(this).hide();
-                        $(this).css({'left': '-212px'});
-                }
-                );
+                playThrowing ();
         })
+
         .mouseup(function(){
-                console.log('mouseup');
-                $('.ryu-throwing').hide();
-                $('.ryu-ready').show();
+            playThrowingFinish ();
         });
+
         $(document).keydown(function(event) {
             playCool ();
         });
 
-        $.when.apply(playCool).done(function(){
-            $('.ryu-cool').hide();
-            $('.ryu-ready').show();
+        $.when(playCool).done(function(){
+            playCoolFinish ();
         });
 
-        $('#directions').show().delay(3000).fadeOut();
-        });
+        directions ();
+
+});
+
+// ********AUDIO FUNCTIONS*******************
 
 function playHadouken () {
   $('.hadouken-sound')[0].volume = 0.5;
